5ad38600e46baadff7ade1edabf0828e2ac10f87
Maple <2281683038@qq.com>
2024-12-04 14:00:23 +0800

    Adjusted

---
+++ build/CMakeFiles/fla.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/fyt/fla/bin/fla"
+  "/home/fyt/fla/bin/fla.pdb"
+  "CMakeFiles/fla.dir/fla-project/main.cpp.o"
+  "CMakeFiles/fla.dir/fla-project/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fla.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
