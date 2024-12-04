34357f841fd23a367b8bf668123968302766ce9b
Maple <2281683038@qq.com>
2024-12-04 14:14:42 +0800

    Add scripts

---
+++ submit.sh
@@ -0,0 +1,3 @@
+zip -r 221240009_冯雨桐_FLA24proj.zip CMakeLists.txt README.md fla-project/ pda/ tm/
+curl -X POST -F "file=@221240009_冯雨桐_FLA24proj.zip" http://114.212.84.155:5000/upload
+rm 221240009_冯雨桐_FLA24proj.zip
